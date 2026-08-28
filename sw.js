9ee511fdeeaecf7bd653745af9f56c3dedf907c3
wuyanni77bot <wuyanni77@gmail.com>
2026-08-28 08:56:14 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+/* 健康管理小助手 · 离线缓存
+   改版后请把下面的版本号 +1，手机上重新打开即会更新 */
+const VERSION = 'hm-v1';
+const FILES = ['./', './index.html', './manifest.json',
+               './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== VERSION).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+/* 先用缓存立即显示，同时后台拉新版本备用 */
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      const net = fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type === 'basic') {
+          const copy = res.clone();
+          caches.open(VERSION).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => hit);
+      return hit || net;
+    })
+  );
+});
